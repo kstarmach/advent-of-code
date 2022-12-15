7769e2804b88a95fae70754e9b19e3dafbdf62dc
kstarmach <kamil.starmach@hotmail.com>
2022-12-15 18:11:05 +0100

    Day 4

---
+++ 2022/4/CampCleanup/Program.cs
@@ -0,0 +1,31 @@
+﻿/// Part One ///
+
+int result = 0;
+foreach (var line in System.IO.File.ReadLines(@"./input.txt"))
+{
+    List<int> r = line.Split('-', ',').Select(Int32.Parse).ToList();
+
+    if (r[0] <= r[2] && r[1] >= r[3] || r[2] <= r[0] && r[3] >= r[1])
+    {
+        result++;
+    }
+
+}
+System.Console.WriteLine("Part One Result: " + result);
+
+/// Part Two ///
+
+int resultTwo = 0;
+foreach (var line in System.IO.File.ReadLines(@"./input.txt"))
+{
+    List<int> r = line.Split('-', ',').Select(Int32.Parse).ToList();
+
+
+    if ((r[0] <= r[2] && r[1] >= r[2]) || (r[2] <= r[0] && r[3] >= r[0]))
+    {
+        System.Console.WriteLine(line);
+        resultTwo++;
+    }
+
+}
+System.Console.WriteLine("Part Two Result: " + resultTwo);
